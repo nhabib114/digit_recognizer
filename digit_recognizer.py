3c0850e1718e810d9ccd682dd96c447f7e3d2053
Nazia Habib <habib.nazia@gmail.com>
2016-05-02 21:29:44 -0500

    file update

--- digit_recognizer.py
+++ digit_recognizer.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[19]:
+# In[103]:
 
 import numpy as np
 import pandas as pd
@@ -9,97 +9,96 @@ import csv as csv
 from IPython.display import display
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+#get_ipython().magic(u'matplotlib inline')
 
 from sklearn import datasets, svm, metrics
 
 
-# In[20]:
+# In[104]:
 
 in_file = 'train.csv'
 digits = pd.read_csv(in_file)
 
 
-# In[21]:
+# In[105]:
 
 display(digits.head())
 
 
-# In[22]:
+# In[106]:
+
+n_samples = digits.shape[0]
+digits_train = digits[:10000]
+
+#in_file = 'test.csv'
+#digits_test = pd.read_csv(in_file)
+
+digits_test = digits[n_samples / 2:]
+
+display(digits_test.head())
+
+
+# In[107]:
 
 # Extract feature (X) and target (y) columns
-feature_cols = list(digits.columns[1:])  # all columns but first are features
-target_col = digits.columns[0]  # first column is the target/label
+feature_cols = list(digits_train.columns[1:])  # all columns but first are features
+target_col = digits_train.columns[0]  # first column is the target/label
 #print "Feature column(s):-\n{}".format(feature_cols)
 print "Target column: {}".format(target_col)
 
-X_train = digits[feature_cols]  # feature values for all students
-y_train = digits[target_col]  # corresponding targets/labels
+X_train = digits_train[feature_cols]  # feature values 
+y_train = digits_train[target_col]  # corresponding targets/labels
 print "\nFeature values:-"
-print X_train.head()  # print the first 5 rows
+print X_train.head() 
 
 
-# In[23]:
+# In[108]:
 
-in_file = 'test.csv'
-test_digits = pd.read_csv(in_file)
-feature_cols = list(test_digits.columns[0:])
-X_test = test_digits[feature_cols]
+# Extract feature (X) and target (y) columns
+feature_cols = list(digits_test.columns[1:])  # all columns but first are features
+target_col = digits_test.columns[0]  # first column is the target/label
+#print "Feature column(s):-\n{}".format(feature_cols)
+print "Target column: {}".format(target_col)
 
-print X_test.head()  # print the first 5 rows
-
-
-# In[24]:
-
-samparr = test_digits[0:1].as_matrix()
-samparr.shape = (28,28)
-plt.imshow(samparr, cmap = "Greys")
+X_test = digits_test[feature_cols]  # feature values 
+y_test = digits_test[target_col]  # corresponding targets/labels
+print "\nFeature values:-"
+print X_test.head() 
 
 
-# In[25]:
+# In[109]:
 
 from sklearn.decomposition import PCA
+from sklearn.preprocessing import normalize
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
 
 pca = PCA(n_components=100)
 X_transformed = pca.fit_transform(X_train)
+#X_norm = normalize(X_train)
 
 
-# In[26]:
+# In[110]:
 
-clf = GaussianNB()
+clf = SVC(kernel='poly', degree=2)
 clf.fit(X_transformed, y_train)
+#clf.fit(X_norm, y_train)
 
 
-# In[27]:
+# In[111]:
 
 X_test_transformed = pca.transform(X_test)
+#X_test_norm = normalize(X_test)
 
 
-# In[28]:
+# In[112]:
 
 pred = clf.predict(X_test_transformed)
-
-
-# In[29]:
-
-prediction_file = open('pred.csv', 'wb')
-prediction_file_object = csv.writer(prediction_file)
-prediction_file_object.writerow(["ImageId", "Label"])
-
-
-# In[30]:
-
-for number, i in enumerate(pred):
-    prediction_file_object.writerow([number + 1, i])
-
-
-# In[31]:
-
-prediction_file.close()
-
-
-# In[ ]:
+#pred = clf.predict(X_test_norm)
+acc = accuracy_score(y_test, pred)
+print acc
 
 
 
